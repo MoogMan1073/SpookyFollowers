c7f2ae9bead77dd11c5b45fe380c9b6dc8436ed0
Eli <emoorhouse@ezsystemsllc.com>
2020-09-17 21:42:46 -0400

    Added file that includes initial servo control

--- servoCTRL.py
+++ servoCTRL.py
@@ -1,26 +1,55 @@
+# TODO: develop SpookyFollower class with servo, position, and angle information
+# TODO: add light sensor to take a new static_back image if light intensity changes too much
+# TODO: Add way to toggle program on/off (physical switch, input from ESP32, etc)
+
 # importing OpenCV, time and Pandas library
-import cv2, time, pandas
+import cv2, time
 # importing datetime class from datetime library
 from datetime import datetime
 
+#Servo Imports
+import RPi.GPIO as GPIO
+import time
 
-print(cv2.getVersionString())
+# CONFIG
+MIN_POS= 480
+MAX_POS = 11
+
+MIN_ANG = 0
+MAX_ANG = 25
+
+POS_UPDATE_OFFSET = 10
+
+def translate(value, MinPos, MaxPos, MinAng, MaxAng):
+    PosSpan = MaxPos - MinPos
+    AngSpan = MaxAng - MinAng
+
+    valueScaled = float(value - MinPos) / float(PosSpan)
+    angle = MinAng + (valueScaled * MaxAng)
+    return (2+(angle/18))
+
+
+# Set GPIO numbering mode
+GPIO.setmode(GPIO.BOARD)
+
+# Set pin 11 as an output, and define as servo1 as PWM pin
+GPIO.setup(8,GPIO.OUT)
+servo1 = GPIO.PWM(8, 50) # pin 11 for servo1, pulse 50Hz
+
+# Start PWM running, with value of 0 (pulse off)
+servo1.start(0)
+
 # Assigning our static_back to None
 static_back = None
 
-# List when any moving object appear
-motion_list = [None, None]
-
-# Time of movement
-time = []
-
-# Initializing DataFrame, one column is start
-# time and other column is end time
-df = pandas.DataFrame(columns=["Start", "End"])
 
 # Capturing video
 video = cv2.VideoCapture(0)
 video.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.0)
+print('get dafuq out da way fam')
+time.sleep(3)
+
+x_stored = 0
 
 # Infinite while loop to treat stack of image as video
 while True:
@@ -54,7 +83,7 @@ while True:
 
     rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 30))
     threshed = cv2.morphologyEx(thresh_frame, cv2.MORPH_CLOSE, rect_kernel)
-    cv2.imshow('threshed', threshed)
+    # cv2.imshow('threshed', threshed)
 
     cnts, _ = cv2.findContours(threshed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
@@ -64,8 +93,10 @@ while True:
 
     a = 0
     for contour in cnts:
+    #if cnts:
+
         hull = cv2.convexHull(contour)
-        print(cv2.contourArea(hull))
+        # print(cv2.contourArea(hull))
         if cv2.contourArea(hull) < 8000:
             target = hull
             continue
@@ -74,32 +105,29 @@ while True:
         (x, y, w, h) = cv2.boundingRect(hull)  # changed from contour
         # making green rectangle around the moving object
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-        cv2.putText(frame, 'object ' + str(a), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 3)
+        cv2.putText(frame, 'fucc boi ' + str(a), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 3)
         # print(x)
+
+        if (x > x_stored + POS_UPDATE_OFFSET or x < x_stored - POS_UPDATE_OFFSET):
+            servoPos = translate(x, MIN_POS, MAX_POS, MIN_ANG, MAX_ANG)
+            servo1.ChangeDutyCycle(servoPos)
+            # time.sleep(1)
+            # servo1.ChangeDutyCycle(0)
+            x_stored = x
+            print(x)
         a += 1
-        # Appending status of motion
-    motion_list.append(motion)
 
-    motion_list = motion_list[-2:]
-
-    # Appending Start time of motion
-    if motion_list[-1] == 1 and motion_list[-2] == 0:
-        time.append(datetime.now())
-
-        # Appending End time of motion
-    if motion_list[-1] == 0 and motion_list[-2] == 1:
-        time.append(datetime.now())
 
         # Displaying image in gray_scale
-    cv2.imshow("Gray Frame", gray)
+    # cv2.imshow("Gray Frame", gray)
 
     # Displaying the difference in currentframe to
     # the staticframe(very first_frame)
-    cv2.imshow("Difference Frame", diff_frame)
+    # cv2.imshow("Difference Frame", diff_frame)
 
     # Displaying the black and white image in which if
     # intensity difference greater than 30 it will appear white
-    cv2.imshow("Threshold Frame", thresh_frame)
+    # cv2.imshow("Threshold Frame", thresh_frame)
 
     # Displaying color frame with contour of motion of object
     cv2.imshow("Color Frame", frame)
@@ -107,17 +135,7 @@ while True:
     key = cv2.waitKey(1)
     # if q entered whole process will stop
     if key == ord('q'):
-        # if something is movingthen it append the end time of movement
-        if motion == 1:
-            time.append(datetime.now())
         break
-
-# Appending time of motion in DataFrame
-for i in range(0, len(time), 2):
-    df = df.append({"Start": time[i], "End": time[i + 1]}, ignore_index=True)
-
-# Creating a CSV file in which time of movements will be saved
-df.to_csv("Time_of_movements.csv")
 
 video.release()
 
